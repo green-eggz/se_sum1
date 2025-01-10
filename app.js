67c6aef530d6b4ae5585ae1d511e0dfc40855df7
green-eggz <121009513+green-eggz@users.noreply.github.com>
2025-01-10 15:46:15 +0000

    Use Font Awesome to display icons for weather

--- app.js
+++ app.js
@@ -33,6 +33,37 @@ const weatherCodeMap = {
     
 };
 
+// Map weather codes to icons
+const weatherIconMap = {
+    0: 'fa-sun',
+    1: 'fa-cloud-sun',
+    2: 'fa-cloud-sun',
+    3: 'fa-cloud-sun',
+    45: 'fa-cloud',
+    48: 'fa-cloud',
+    51: 'fa-cloud-rain',
+    53: 'fa-cloud-rain',
+    55: 'fa-cloud-rain',
+    56: 'fa-cloud-rain',
+    57: 'fa-cloud-rain',
+    61: "fa-cloud-rain",
+    63: 'fa-cloud-rain',
+    65: 'fa-cloud-rain',
+    71: 'fa-snowflake',
+    73: 'fa-snowflake',
+    75: 'fa-snowflake',
+    77: 'fa-snowflake',
+    80: 'fa-cloud-rain',
+    81: 'fa-cloud-rain',
+    82: 'fa-cloud-rain',
+    85: 'fa-snowflake',
+    86: 'fa-snowflake',
+    95: 'fa-bolt-lightning',
+    96: 'fa-cloud-lightning',
+    99: 'fa-cloud-lightning'
+    
+};
+
 // URL for weather data
 const apiUrl = `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current_weather=true`;
 
@@ -44,11 +75,16 @@ fetch(apiUrl)
         const temperature = data.current_weather.temperature; // Temperature in Celsius
         const weatherCode = data.current_weather.weathercode; // Weather condition code
         const weatherDescription = weatherCodeMap[weatherCode] // Map weather code to description
+        const weatherIcon = weatherIconMap[weatherCode] // Map weather code to icon
 
         // Update the HTML with the fetched data
+        document.getElementById('coordinates').textContent = `Latitude: ${latitude}, Longitude: ${longitude}`;
         document.getElementById('temperature').textContent = `Temperature: ${temperature}°C`;
         document.getElementById('weather-description').textContent = `Weather: ${weatherDescription}`;
-        document.getElementById('coordinates').textContent = `Latitude: ${latitude}, Longitude: ${longitude}`;
+        
+        const iconElement = document.getElementById('weather-icon');
+        iconElement.classList.add('fas', weatherIcon);
+
     })
     .catch(error => console.error('Error:', error));  // Log any errors that occur
 
